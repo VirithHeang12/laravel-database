f5c8ef13f468af95c44696620d5b54d3bc204508
Heang Virith <virith.id@gmail.com>
2024-10-29 22:29:02 +0700

    Refactor blade templates to add supplier link in navigation

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -29,6 +29,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('books.index') }}">Books</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('suppliers.index') }}">Suppliers</a>
+                    </li>
                 </ul>
             </div>
         </nav>
