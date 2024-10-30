2829c17a2582a01550219f007a4431e327dc362f
Cheth Chivy <chethchivy015@gmail.com>
2024-10-30 13:48:25 +0700

    Edit some style and add the transaction on customers

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -2,12 +2,32 @@
 
 @section('content')
     <div class="row justify-content-center">
+        <h1 class="my-4 text-center">Customer Detail</h1>
         <div class="col-lg-4 col-md-6 col-12">
-            <h1 class="my-4 text-center">Customer Detail</h1>
-            <h6>Customer Name : {{ $customer->name }}</h6>
-            <h6>Phone Number : {{ $customer->phone }}</h6>
-            <h6>Created at : {{ $customer->created_at }}</h6>
-            <h6>Updated at : {{ $customer->updated_at }}</h6>
+        <table class="table">
+                <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <td>{{ $customer->id }}</td>
+                    </tr>
+                    <tr>
+                        <th>Customer Name :</th>
+                        <td>{{ $customer->name }}</td>
+                    </tr>
+                    <tr>
+                        <th>Phone Number :</th>
+                        <td>{{ $customer->phone }}</td>
+                    </tr>
+                    <tr>
+                        <th>Created at :</th>
+                        <td>{{ $customer->created_at }}</td>
+                    </tr>
+                    <tr>
+                        <th>Updated at :</th>
+                        <td>{{ $customer->updated_at }}</td>
+                    </tr>
+                </tbody>
+            </table>
             <a class="btn btn-primary px-4 mt-4" href="{{ route('customers.index') }}">Back</a>
         </div>
     </div>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -32,6 +32,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('suppliers.index') }}">Suppliers</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('customers.index') }}">Customers</a>
+                    </li>
                 </ul>
             </div>
         </nav>
